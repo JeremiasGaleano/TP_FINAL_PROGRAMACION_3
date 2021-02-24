94237bfd8d7347aec3c6c35f055e3fb03e04a526
JeremiasGaleano <63808502+JeremiasGaleano@users.noreply.github.com>
2021-02-24 13:55:26 -0300

    sin cambios
    h

---
+++ 1111/Entidades/Presupuestos.cs
@@ -0,0 +1,62 @@
+﻿using System;   
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Presupuestos
+    {
+        //escribo todos los campos que va a tener la entidad.
+
+        string nombre;//completado
+        string telefono_fijo;//completado
+        string celular;//completado
+        string email;//completado
+        string consulta;//completado
+
+        //declaro un constructor vacio.
+        public Presupuestos() { }
+
+        public void set_nombre(string nuevo_nombre)
+        {
+            this.nombre = nuevo_nombre;
+        }
+
+        public string get_nombre()
+        {
+            return this.nombre;
+        }
+
+        public void set_telefono_fijo(string nuevo_telefono_fijo)
+        {
+            this.telefono_fijo = nuevo_telefono_fijo;
+        }
+
+        public string get_telefono_fijo()
+        {
+            return this.telefono_fijo;
+        }
+
+        public void set_email(string nuevo_email)
+        {
+            this.email = nuevo_email;
+        }
+
+        public string get_email()
+        {
+            return this.email;
+        }
+
+        public void set_consulta(string nueva_consulta)
+        {
+            this.consulta = nueva_consulta;
+        }
+
+        public string get_consulta()
+        {
+            return this.consulta;
+        }
+    }
+}
---
+++ 1111/Entidades/Productos.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    //escribo todos los campos que va a tener la entidad.
+    public class Productos
+    {
+         string Cod_Art_Pro; //completado
+         decimal PU_Pro; //complteado
+         decimal Stock; //complteado
+         string Categoria; //complteado
+         string Descripcion; //complteado
+         bool Estado_Producto; //complteado    
+         string Url_Imagen; //complteado  
+
+        public void Cod_Art(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        // constructor
+        public Productos()
+        {
+
+        }
+        
+        // getter & setter  ===== Codigo de articulo 
+
+        public string getCod_Art_Pro()
+        {
+            return Cod_Art_Pro;
+        }
+        public void setCod_Art_Pro(string cod_Art_Pro)
+        {
+            Cod_Art_Pro = cod_Art_Pro;
+        }
+
+        // getter & setter  ===== Precio unitario 
+
+        public decimal getPU_Pro()
+        {
+            return PU_Pro;
+        }
+        public void setPU_Pro(decimal pU_Pro)
+        {
+            PU_Pro = pU_Pro;
+        }
+
+        // getter & setter  ===== Stock
+
+        public decimal getStock()
+        {
+            return Stock;
+        }
+        public void setStock(decimal stock)
+        {
+            Stock = stock;
+        }
+
+        // getter & setter  ===== Categoria
+
+        public string getCategoria()
+        {
+            return Categoria;
+        }
+        public void setCategoria(string categoria)
+        {
+            Categoria = categoria;
+        }
+
+        // getter & setter  ===== Descripcion
+
+        public string getDescripcion()
+        {
+            return Descripcion;
+        }
+        public void setDescripcion(string descripcion)
+        {
+            Descripcion = descripcion;
+        }
+
+        // getter & setter  ===== Estado_Producto
+
+        public bool getEstado_Producto()
+        {
+            return Estado_Producto;
+        }
+        public void setEstado_Producto(bool estado_Producto)
+        {
+            Estado_Producto = estado_Producto;
+        }
+
+        // getter & setter  ===== Imagen
+
+        public string getUrl_Imagen()
+        {
+            return Url_Imagen;
+        }
+        public void setUrl_Imagen(string url_Imagen)
+        {
+            Url_Imagen = url_Imagen;
+        }
+
+    }
+}
